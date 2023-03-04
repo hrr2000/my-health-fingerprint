d2b662bc08cd5e8f5b473c8668d87e756e5e7474
Destro45 <Kerolous Salah>
2023-03-04 20:05:10 +0200

    added animation among pages

--- src/components/toolbox/routes.ts
+++ src/components/toolbox/routes.ts
@@ -1,20 +1,20 @@
 import { type ILink } from "./types";
 
-export const routes : {
-  landingPages: ILink[]
+export const routes: {
+  landingPages: ILink[];
 } = {
   landingPages: [
     {
-      href: "featuers", 
-      display_text: "Features"
+      href: "features",
+      display_text: "Features",
     },
     {
-      href: "how-to-apply", 
-      display_text: "How to Apply?"
+      href: "how-to-apply",
+      display_text: "How to Apply?",
     },
     {
       href: "vission",
-      display_text: "Vission"
-    }
-  ]
-}
+      display_text: "Vission",
+    },
+  ],
+};
