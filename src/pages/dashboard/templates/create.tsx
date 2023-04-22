a73c97325de132d5d5c928a7e7fd2636bbc3f6b1
hrr2000 <thedragonea@gmail.com>
2023-04-22 02:21:59 +0200

    prepared a placeholder design for the builder

--- src/pages/dashboard/templates/create.tsx
+++ src/pages/dashboard/templates/create.tsx
@@ -5,6 +5,8 @@ import {
 } from "next";
 import { getServerAuthSession } from "@/server/auth";
 import DashBoardLayout from "@/layouts/DashboardLayout";
+import { IoIosAdd } from "react-icons/io";
+import { MdModeEditOutline } from 'react-icons/md';
 type serverSidePropsType = NextPage<
   InferGetServerSidePropsType<typeof getServerSideProps>
   >;
@@ -14,7 +16,41 @@ const DashboardPage: serverSidePropsType = ({ user }) => {
   return (
     <DashBoardLayout user={user} title="" description="">
       <main className="relative grid h-full grid-cols-[1fr_350px]">
-        <section className="flex border-r-[1px] border-slate-200 text-black">
+        <section className="flex p-5 border-r-[1px] border-slate-200 text-black">
+          <div className={`w-full border-slate-200 border-[1px] p-5 gap-3 flex flex-col`}>
+            <div className={`border-slate-200 w-full grid grid-cols-12 justify-center border-[1px] min-h-[50px]`}>
+              <div className={`border-slate-200 border-[1px] col-span-4`}>
+                <button className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px] gap-2`}>
+                  <span className={`text-xs`}>
+                    Patient Field
+                  </span>
+                  <MdModeEditOutline size={15} />
+                </button>
+              </div>
+              <div className={`border-slate-200 border-[1px] col-span-4`}>
+                <button className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px]`}>
+                  <IoIosAdd size={20} />
+                  <span className={`text-xs`}>
+                    Add A Field
+                  </span>
+                </button>
+              </div>
+              <div className={`border-slate-200 border-[1px] col-span-4`}>
+                <button className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px]`}>
+                  <IoIosAdd size={20} />
+                  <span className={`text-xs`}>
+                    Add A Field
+                  </span>
+                </button>
+              </div>
+            </div>
+            <button className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px]`}>
+              <IoIosAdd size={20} />
+              <span className={`text-xs`}>
+                Add A Row
+              </span>
+            </button>
+          </div>
         </section>
         <section className="flex flex-col border-slate-200 text-black p-5">
           <div className={`w-full py-2 px-4`}>
