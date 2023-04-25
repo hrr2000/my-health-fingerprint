82101275ad31dc2378f290b9e1b536c797494206
hrr2000 <thedragonea@gmail.com>
2023-04-25 15:43:33 +0200

    testing the modal

--- src/contexts/TemplateBuilderContext.tsx
+++ src/contexts/TemplateBuilderContext.tsx
@@ -1,4 +1,5 @@
 import {createContext, Dispatch, ReactNode, SetStateAction, useState} from 'react';
+import Modal from 'react-modal';
 
 export const templateDetailsInitialValues = {
   name: "",
@@ -56,6 +57,21 @@ export function TemplateBuilderContextProvider({children}: {children: ReactNode}
   const [templateDetails, setTemplateDetails] = useState<TemplateDetails>(templateDetailsInitialValues);
   const [collectionDetails, setCollectionDetails] = useState<CollectionDetails>(collectionDetailsInitialValues);
 
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   const appendRow = (columnsCount: number) => {
     if(!columnsCount || columnsCount > 4) return;
     const row = [];
@@ -87,7 +103,16 @@ export function TemplateBuilderContextProvider({children}: {children: ReactNode}
       schema: tmpSchema
     });
   }
-
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
   return (
     <TemplateBuilderContext.Provider value={{
       templateDetails,
@@ -99,6 +124,27 @@ export function TemplateBuilderContextProvider({children}: {children: ReactNode}
       updateColumn
     }}>
       {children}
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          style={customStyles}
+          contentLabel="Example Modal"
+        >
+          <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+          <form>
+            <input />
+            <button>tab navigation</button>
+            <button>stays</button>
+            <button>inside</button>
+            <button>the modal</button>
+          </form>
+        </Modal>
+      </div>
     </TemplateBuilderContext.Provider>
   )
 }
