574950cb07705801e23c468c085d8cacc2adbc8f
hrr2000 <thedragonea@gmail.com>
2023-04-26 14:51:38 +0200

    column with modal

--- src/components/templates/TemplateBuilder/engine/Column.tsx
+++ src/components/templates/TemplateBuilder/engine/Column.tsx
@@ -1,13 +1,18 @@
 import {IoIosAdd} from "react-icons/io";
-import {type TemplateComponent} from "@/contexts/TemplateBuilderContext";
+import {type TemplateComponent, useTemplateBuilder} from "@/contexts/TemplateBuilderContext";
 
 export default function Column({rowIndex, columnIndex, gridSpan}: {rowIndex: number; columnIndex: number; gridSpan: number; item: Partial<TemplateComponent>}) {
+  const {openModal} = useTemplateBuilder();
 
   return (
     <div  className={`border-slate-200 border-[1px]`} style={{
       gridColumn: `span ${gridSpan} / span ${gridSpan}`
     }}>
-      <button className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px]`}>
+      <button
+        onClick={() => {
+          openModal();
+        }}
+        className={`border-slate-200 w-full hover:shadow-md duration-300 flex items-center justify-center border-[1px] min-h-[50px]`}>
         <IoIosAdd size={20} />
         <span className={`text-xs`}>
           Add A Field
