7539fad03b8aa82af5bfbe2f80c9a85d0c08b790
Kokosalah45 <kokp.amged@gmail.com>
2023-05-18 15:06:56 +0300

    added new models for the databse

--- src/server/models/index.ts
+++ src/server/models/index.ts
@@ -3,10 +3,14 @@ import PermissionModel from "./permission";
 import OrganizationModel from "./organization";
 import AllowedPermissionModel from "./allowed-permission";
 import PatientModel from "./patient";
+import custom_collections from "./custom-collection";
+import collections_templates from "./collections-templates";
 export {
   UserModel,
   PatientModel,
   PermissionModel,
   OrganizationModel,
   AllowedPermissionModel,
+  custom_collections,
+  collections_templates,
 };
