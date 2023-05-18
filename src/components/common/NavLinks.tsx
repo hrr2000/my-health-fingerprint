bef32782bd797f666986ca8be421c12d73badf38
Kokosalah45 <kokp.amged@gmail.com>
2023-05-18 15:04:09 +0300

    created flexible navbar component

---
+++ src/components/common/NavLinks.tsx
@@ -0,0 +1,41 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { type GenericProps, type ILink } from "@/types/application";
+import Image from "next/image";
+interface LocalProps extends GenericProps {
+  links: ILink[];
+  activeLinkClassName: string;
+  linkClassName: string;
+  className: string;
+}
+
+export function NavLinks({
+  links,
+  activeLinkClassName,
+  linkClassName,
+  className,
+}: LocalProps) {
+  const router = useRouter();
+
+  return (
+    <ul className={`${className}`}>
+      {links.map(({ href, image, display_text }) => (
+        <li
+          key={href}
+          className={` ${linkClassName} ${
+            router.asPath === "/" + href ? activeLinkClassName : ""
+          }`}
+        >
+          <Link className="flex w-full items-center gap-4" href={`/${href}`}>
+            {typeof image === "string" ? (
+              <Image src={image} alt="" width={30} height={30} />
+            ) : (
+              image
+            )}
+            {display_text}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
