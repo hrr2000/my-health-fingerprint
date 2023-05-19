190e0a592c412faab9e743a97289001d3aa3e3dc
Kokosalah45 <kokp.amged@gmail.com>
2023-05-19 22:51:47 +0300

    feature:update collection

--- src/validation/custom-collection.ts
+++ src/validation/custom-collection.ts
@@ -1,3 +1,4 @@
+import { isValidObjectId } from "mongoose";
 import { z } from "zod";
 import { toFormikValidationSchema } from "zod-formik-adapter";
 
@@ -8,7 +9,7 @@ export const createCollectionSchema = z.object({
     name: z.string().trim().min(3).max(45),
     isPublic: z.boolean(),
     isPatientProfile: z.boolean(),
-    description: z.string()
+    description: z.string(),
   }),
   template: z.object({
     schema: z.any(),
@@ -16,10 +17,31 @@ export const createCollectionSchema = z.object({
     isPrintable: z.boolean(),
   }),
 });
+export const updateCollectionSchema = z.object({
+  collection: z.object({
+    name: z.string().trim().min(3).max(45),
+    isPublic: z.boolean(),
+    isPatientProfile: z.boolean(),
+    description: z.string(),
+  }),
+  template: z.object({
+    schema: z.any(),
+    name: z.string(),
+    isPrintable: z.boolean(),
+  }),
+  slug: z
+    .string()
+    .trim()
+    .refine((val) => isValidObjectId(val), `Input must be an ObjectId`),
+});
 // this is for client-side validation
 export const createCollectionFormSchema = toFormikValidationSchema(
+  createCollectionSchema
+);
+export const updateCollectionFormSchema = toFormikValidationSchema(
   createCollectionSchema
 );
 
 // for general purposes -> server validation , usage in services , uses in application itself
 export type createCollectionType = z.infer<typeof createCollectionSchema>;
+export type updateCollectionType = z.infer<typeof updateCollectionSchema>;
