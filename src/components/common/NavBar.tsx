7612f96f545d7ebb0b91fa9801fbf5a5ad7d8e79
Kokosalah45 <kokp.amged@gmail.com>
2023-05-25 15:04:28 +0300

    bug:sidebar on small screen disappear fixed

--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -2,20 +2,25 @@ import { type ReactElement } from "react";
 import { type GenericProps } from "@/types/application";
 import { NavLinks } from "@/components/common/NavLinks";
 
+type ModeOptions = "mobile" | "pc" | "both";
 interface LocalProps extends GenericProps {
   className: string;
   children: ReactElement<typeof NavLinks>;
-  mode?: "mobile" | "pc";
+  mode: ModeOptions;
 }
 
-export function NavBar({ children, className, mode }: LocalProps) {
+const checkMode = (mode: ModeOptions): string => {
+  if (mode === "both") {
+    return "";
+  }
   const [smallScreenState, largeScreenState] =
     mode === "mobile" ? ["block", "lg:hidden"] : ["hidden", "lg:block"];
-  return (
-    <nav className={`${className} ${smallScreenState} ${largeScreenState}`}>
-      {children}
-    </nav>
-  );
+
+  return [smallScreenState, largeScreenState].join(" ");
+};
+
+export function NavBar({ children, className, mode }: LocalProps) {
+  return <nav className={`${className} ${checkMode(mode)}`}>{children}</nav>;
 }
 
 NavBar.NavLinks = NavLinks;
