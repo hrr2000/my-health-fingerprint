2345f3d3588c7424ecde412e4c524d0706414f3d
Kokosalah45 <kokp.amged@gmail.com>
2023-06-08 17:16:37 +0300

    fix:some type errors

--- src/hooks/useGetHealthRecordData.ts
+++ src/hooks/useGetHealthRecordData.ts
@@ -4,20 +4,13 @@ export default function useGetHealthRecordData(
   patientId: string,
   requestedData: "registered_collections" | "unregistered_collections" | ""
 ) {
-  let dataObj = null;
-  dataObj = api.patient.getRegisteredCollections.useQuery(
+  const dataObj = api.patient.getRegisteredCollections.useQuery(
     { nationalId: patientId },
     {
       enabled: !!patientId && requestedData === "registered_collections",
       retry: 1,
     }
   );
-  // dataObj = api.patient.getUnRegisteredCollections.useQuery(
-  //   { registeredCollections: [] },
-  //   {
-  //     enabled: !!patientId && requestedData === "unregistered_collections",
-  //     retry: 1,
-  //   }
-  // );
+
   return { dataObj };
 }
