c5036c5e731bc12643fc8f316fae3280b97e09a3
Kokosalah45 <kokp.amged@gmail.com>
2023-06-09 23:17:28 +0300

    feature:ability to search in patient record

--- src/components/templates/TemplateBuilder/views/ReadView.tsx
+++ src/components/templates/TemplateBuilder/views/ReadView.tsx
@@ -1,25 +1,49 @@
-export default function ReadView({ data }: { data: { _id: string }[] }) {
+interface IRow {
+  [k: string]: string;
+  _id: string;
+}
+
+const Record = ({ row }: { row: IRow }) => {
+  return (
+    <ul className="cursor-pointer rounded-md border-2 bg-white p-3 transition hover:-translate-x-1 hover:-translate-y-1 hover:bg-gray-100">
+      {Object.entries(row).map(([k, v]) => {
+        return (
+          k !== "_id" && (
+            <li key={k}>
+              <span className="font-semibold capitalize">{k}</span>{" "}
+              <span className="mx-1">:</span> <span>{v}</span>
+            </li>
+          )
+        );
+      })}
+    </ul>
+  );
+};
+
+export default function ReadView({
+  data,
+  fieldName,
+  fieldValue,
+}: {
+  data: IRow[];
+  fieldName: string;
+  fieldValue: string;
+}) {
   // implementation for now
 
   return (
-    <div className="text-primary flex flex-col gap-3">
-      {data?.map((row) => (
-        <ul
-          className="cursor-pointer rounded-md bg-white p-3 border-2 transition hover:-translate-x-1 hover:-translate-y-1 hover:bg-gray-100"
-          key={row._id.toString()}
-        >
-          {Object.entries(row).map(([k, v]) => {
-            return (
-              k !== "_id" && (
-                <li key={k}>
-                  <span className="font-semibold capitalize">{k}</span>{" "}
-                  <span className="mx-1">:</span> <span>{v}</span>
-                </li>
-              )
-            );
-          })}
-        </ul>
-      ))}
+    <div className="flex flex-col gap-3 text-primary">
+      {data?.map((row) =>
+        !fieldName || !fieldValue ? (
+          <Record key={row._id.toString()} row={row} />
+        ) : (
+          row[fieldName]
+            .toLowerCase()
+            .includes(fieldValue.toLowerCase().trim()) && (
+            <Record key={row._id.toString()} row={row} />
+          )
+        )
+      )}
     </div>
   );
 }
