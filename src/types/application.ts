56889d79354405b6c99a7acec8ec69c673a06fe4
Kokosalah45 <kokp.amged@gmail.com>
2023-06-10 19:38:39 +0300

    feature:pages authX

--- src/types/application.ts
+++ src/types/application.ts
@@ -1,23 +1,29 @@
-import { type ReactNode } from "react";
-import { type Session } from "next-auth";
+import {type ReactNode} from "react";
+import {type Session} from "next-auth";
+
+export type PageEntititesType = 'collections' | 'settings' | 'patients' | 'users' | 'roles' | 'organizations'
+
 export interface ILink {
-  href: string;
-  image?: ReactNode | string;
-  display_text: string;
+    entity?: PageEntititesType
+    href: string;
+    image?: ReactNode | string;
+    display_text: string;
 }
 
 export interface Meta {
-  name: string;
-  content: string;
+    name: string;
+    content: string;
 }
+
 type FormValues = (values: Record<string, string>) => boolean;
+
 export interface GenericProps {
-  [k: string]:
-    | ReactNode
-    | ILink[]
-    | Meta[]
-    | Session["user"]
-    | (() => void | React.ReactNode)
-    | Record<string, string>
-    | FormValues;
+    [k: string]:
+        | ReactNode
+        | ILink[]
+        | Meta[]
+        | Session["user"]
+        | (() => void | React.ReactNode)
+        | Record<string, string>
+        | FormValues;
 }
