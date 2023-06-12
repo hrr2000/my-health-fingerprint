9ffb7a901e0bbedf870a886534e3487001692046
BraveCake <aliotaku241@gmail.com>
2023-06-12 06:09:17 +0300

    update the content of feautures & vision pages

--- src/pages/features.tsx
+++ src/pages/features.tsx
@@ -1,21 +1,58 @@
-import { type NextPage } from "next";
+import { NextPage } from "next";
 import PageLayout from "@/layouts/PageLayout";
 import MainLayout from "@/layouts/MainLayout";
 
 const Home: NextPage = () => {
+  const softwareGoals = [
+    "Ease of Use",
+    "Ease of signing up however accompanied with high emphasis on valid docs",
+    "Ease of onboarding process for employees",
+    "Ease and speed of information access and navigation, almost intuitive",
+    "Accessibility is key",
+  ];
+
+  const patientGoals = [
+    "Safety of accessing their information and making sure it's not getting abused",
+    "Foolproof methods for identity validation",
+    "Ease of tracking patient history",
+  ];
+
+  const securityGoals = [
+    "Built with fine-grained authorization as its main ideology",
+    "Data access tracking",
+    "Applying for usage is not trivial and standard, clear procedures will be defined for acceptance",
+  ];
+
   return (
     <PageLayout title="Home" description="مهفب ءفءح">
       <MainLayout cols="1">
-        Software Goals Regarding Organization owners and Employees 1 - Ease of
-        Use 2 - Ease of signing up however accompanied with high emphasis on
-        valid docs 3 - Ease of on boarding process for employees 4 - Ease and
-        speed of information access and navigation almost intuitive 5 -
-        accessibility is key Regarding Patients 1 - Safety of accessing his
-        information and making sure his info is not getting abused 2 - foolproof
-        methods for identity validation 3 - ease of tracking patient history
-        Regarding security 1 - Built with fine grained authorization as it main
-        ideology 2 - data access tracking 3 - applying for usage is not trivial
-        and standard , clear and procedures will be defined for acceptance
+        <div className="mb-4">
+          <p className="font-bold text-2xl">Software Goals Regarding Organization owners and Employees</p>
+          {softwareGoals.map((goal, index) => (
+            <p key={index} className="pl-4">
+              <span className="font-bold">{index + 1} - </span>
+              {goal}
+            </p>
+          ))}
+        </div>
+        <div className="mb-4">
+          <p className="font-bold text-2xl">Regarding Patients</p>
+          {patientGoals.map((goal, index) => (
+            <p key={index} className="pl-4">
+              <span className="font-bold">{index + 1} - </span>
+              {goal}
+            </p>
+          ))}
+        </div>
+        <div>
+          <p className="font-bold text-2xl">Regarding security</p>
+          {securityGoals.map((goal, index) => (
+            <p key={index} className="pl-4">
+              <span className="font-bold">{index + 1} - </span>
+              {goal}
+            </p>
+          ))}
+        </div>
       </MainLayout>
     </PageLayout>
   );
